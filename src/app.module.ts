1caa0527e3239d3a091a96f6afc6cc593fac5716
Đinh Thị Thanh Bình <thanhbinh57kt@mac-thanh-binh.local>
2024-10-06 21:05:07 +0700

    feat:(src) setup app

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerModule } from './modules/customers/customer.module';
+import { CustomerSeeder } from './modules/customers/customer.seed';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.local.env',
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: configService.get<boolean>('DB_SYNC'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    CustomerModule,
+  ],
+  providers: [CustomerSeeder],
+})
+export class AppModule implements OnModuleInit {
+  constructor(private readonly customerSeeder: CustomerSeeder) {}
+
+  async onModuleInit() {
+    await this.customerSeeder.seed();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('eCommerce example')
+    .setDescription('The eCommerce API description')
+    .setVersion('1.0')
+    .addTag('eCommerce')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
